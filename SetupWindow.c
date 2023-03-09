ea4c273f855076613c5cd1d66b7c15f77bafe042
Robert Tetreault <rrt2850@g.rit.edu>
2023-03-08 22:15:22 -0500

    Made it so that the terminal goes back to canonical mode after exiting. - I accidentally broke that when I checked for the user exiting with ctrl+c

--- SetupWindow.c
+++ SetupWindow.c
@@ -1,10 +1,46 @@
+//
+//  File: SetupWindow.c
+//  Author: Robert Tetreault (rrt2850)
+//
+//  Description: Sets up the window for the snake game and restores everything
+//               on exit.
+//
+// // // // // // // // // // // // // // // // // // // // // // // // // // /
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
-#include <sys/ioctl.h>
+#include <signal.h>
 
-#define ESCAPE '\x1b'
+#define ESC 27
+
+int width, height;
+
+void reset_terminal() {
+    printf("\e[0m"); // Reset all text attributes
+    printf("\e[H");  // Move cursor to top-left
+    printf("\e[2J"); // Clear screen
+}
+
+// Handles when the user exits by ctrl+c or 'q'
+void handle_exit() {
+    reset_terminal();
+
+    // Restore terminal settings
+    struct termios t;
+    tcgetattr(STDIN_FILENO, &t);
+    t.c_lflag |= ICANON | ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t);
+
+    exit(0);
+}
+
+// Handles when the user exits by ctrl+c
+void handle_sigint(int sig) {
+    handle_exit();
+}
 
 int main() {
     // Set terminal into non-canonical mode
@@ -13,28 +49,31 @@ int main() {
     t.c_lflag &= ~(ICANON | ECHO);
     tcsetattr(STDIN_FILENO, TCSANOW, &t);
 
-    // Get terminal dimensions
-    struct winsize ws;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
-
     // Clear screen
     printf("\e[2J");
 
-    // Set background color to green for every spot in the window
-    for (int i = 0; i < ws.ws_row; i++) {
-        for (int j = 0; j < ws.ws_col; j++) {
-            printf("\e[42m "); // Set background color and print a space
-        }
-        printf("\n"); // Move to next row
+    // Get terminal size
+    struct winsize size;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
+    width = size.ws_col;
+    height = size.ws_row;
+
+    // Set background color to green for every cell in the terminal
+    for (int i = 0; i < width * height; i++) {
+        printf("\e[42m ");
     }
 
     // Move cursor to top-left
     printf("\e[H");
 
+    // Set up signal handlers for SIGINT (Ctrl+C) and 'q'
+    signal(SIGINT, handle_sigint);
+    signal(SIGQUIT, handle_exit);
+
     // Move cursor based on arrow keys
     while (1) {
         int c = getchar();
-        if (c == ESCAPE) {
+        if (c == ESC) {
             // Escape sequence detected, check for arrow keys
             int c2 = getchar();
             int c3 = getchar();
@@ -61,16 +100,9 @@ int main() {
         }
         else if (c == 'q') {
             // Quit program on 'q' key
-            printf("\e[0m"); // Reset all text attributes
-            printf("\e[H");  // Move cursor to top-left
-            printf("\e[2J"); // Clear screen
-            break;
+            handle_exit();
         }
     }
-
-    // Restore terminal settings
-    t.c_lflag |= ICANON | ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &t);
 
     return 0;
 }
